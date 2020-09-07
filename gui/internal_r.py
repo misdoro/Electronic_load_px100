15ff9e8e10c00d74ace05fa4d8d0e737564a8410
misdoro <misha@doronin.org>
2020-09-07 22:39:39 +0200

    Improve internal resistance measurement UI, logic and accuracy

--- gui/internal_r.py
+++ gui/internal_r.py
@@ -13,6 +13,7 @@ MODE_PREPARE = 1
 MODE_DROP = 2
 MODE_AFTER = 3
 ACQ_SIZE = 2
+MAX_BAD_ROWS = 3
 
 
 class InternalRTableModel(QAbstractTableModel):
@@ -21,12 +22,8 @@ class InternalRTableModel(QAbstractTableModel):
         self.reset()
 
     def append(self, row):
+        self.beginInsertRows(QModelIndex(), self.rowCount(1), self.rowCount(1))
         self._data = self._data.append(row, ignore_index=True)
-        print(self._data)
-        print(self.rowCount(1))
-        self.beginInsertRows(QModelIndex(),
-                             self.rowCount(1) - 1,
-                             self.rowCount(1) - 1)
         self.endInsertRows()
 
     def data(self, index, role):
@@ -41,7 +38,9 @@ class InternalRTableModel(QAbstractTableModel):
             self._data.drop_duplicates().to_csv(filename)
 
     def reset(self):
+        self.beginResetModel()
         self._data = DataFrame(columns=['step', 'r_a', 'r_b'])
+        self.endResetModel()
 
     def rowCount(self, index):
         return self._data.shape[0]
@@ -74,8 +73,8 @@ class InternalR(QGroupBox):
         settings = QSettings()
 
         self.setChecked(settings.value("InternalR/enabled", True, type=bool))
-        self.measurePeriod.setValue(
-            settings.value("InternalR/period", 0.1, type=float))
+        self.v_period = settings.value("InternalR/period", 0.1, type=float)
+        self.measurePeriod.setValue(self.v_period)
 
     def save_settings(self):
         settings = QSettings()
@@ -94,72 +93,93 @@ class InternalR(QGroupBox):
 
     def reset(self):
         self.acq_steps = []
-        self.idle()
+        self._idle()
         self.tableModel.reset()
 
     def write(self, path, prefix):
         self.tableModel.write(path, prefix)
 
-    def idle(self):
+    def data_row(self, data, row):
+        if not self.isChecked() or not self.v_period:
+            return
+
+        if self._valid_data(data):
+            self._data_loop(data)
+        else:
+            self.ignored_rows += 1
+            if self.ignored_rows > MAX_BAD_ROWS:
+                self._idle()
+
+    def _idle(self):
         self.mode = MODE_IDLE
         self.stateLabel.setText('Idle')
         self.pre_current = 0.
+        self.ignored_rows = 0
         self.pre_acq = []
         self.zero_acq = []
         self.after_acq = []
 
-    def data_row(self, data, row):
-        if data and self.isChecked() and data.lastval('is_on'):
-            if self.mode == MODE_IDLE and data.lastval(
-                    'current') > 0 and self._next_step(
-                        data.lastval('voltage')):
-                self.mode = MODE_PREPARE
-                self.stateLabel.setText('Prepare')
-                self.pre_current = data.lastval('set_current')
-                self.pre_acq.append(data.lastval('voltage'))
-            elif self.mode == MODE_PREPARE:
-                self.pre_acq.append(data.lastval('voltage'))
-                if len(self.pre_acq) >= ACQ_SIZE:
-                    self.mode = MODE_DROP
-                    self.stateLabel.setText('Drop')
-                    self.backend.send_command(
-                        {Instrument.COMMAND_SET_CURRENT: 0.0})
-            elif self.mode == MODE_DROP and data.lastval('current') == 0.:
-                self.zero_acq.append(data.lastval('voltage'))
-                if len(self.zero_acq) >= ACQ_SIZE:
-                    self.mode = MODE_AFTER
-                    self.stateLabel.setText('Ramp-up')
-                    self.backend.send_command(
-                        {Instrument.COMMAND_SET_CURRENT: self.pre_current})
-            elif self.mode == MODE_AFTER and round(
-                    data.lastval('current'), 1) == round(self.pre_current, 1):
-                self.after_acq.append(data.lastval('voltage'))
-                if len(self.after_acq) >= ACQ_SIZE:
-                    self.calc_r()
-                    self.idle()
+    def _data_loop(self, data):
+        if self.mode == MODE_IDLE and data.lastval(
+                'current') > 0 and self._next_step(data.lastval('voltage')):
+            self.mode = MODE_PREPARE
+            self.stateLabel.setText('Prepare')
+            self.pre_current = data.lastval('set_current')
+            self.meas_pre_current = data.lastval('current')
+            self.pre_acq.append(data.lastval('voltage'))
+        elif self.mode == MODE_PREPARE:
+            self.pre_acq.append(data.lastval('voltage'))
+            if len(self.pre_acq) >= ACQ_SIZE:
+                self.mode = MODE_DROP
+                self.stateLabel.setText('Drop')
+                self.backend.send_command(
+                    {Instrument.COMMAND_SET_CURRENT: 0.0})
+        elif self.mode == MODE_DROP and data.lastval('current') == 0.:
+            self.zero_acq.append(data.lastval('voltage'))
+            if len(self.zero_acq) >= ACQ_SIZE:
+                self.mode = MODE_AFTER
+                self.stateLabel.setText('Ramp-up')
+                self.backend.send_command(
+                    {Instrument.COMMAND_SET_CURRENT: self.pre_current})
+        elif self.mode == MODE_AFTER and self._stable_current(data, 0.01):
+            self.after_acq.append(data.lastval('voltage'))
+            if len(self.after_acq) >= ACQ_SIZE:
+                self._calc_r()
+                self._idle()
 
-    def calc_r(self):
+    def _calc_r(self):
         print("-- Internal R --")
         print(self.pre_acq)
         print(self.zero_acq)
         print(self.after_acq)
-        print(self.pre_current)
+        print(self.meas_pre_current)
 
-        r_a = (sum(self.zero_acq) - sum(self.pre_acq)) / (ACQ_SIZE *
-                                                          self.pre_current)
-        r_b = (sum(self.zero_acq) - sum(self.after_acq)) / (ACQ_SIZE *
-                                                            self.pre_current)
+        if self.meas_pre_current > 0:
+            r_a = (sum(self.zero_acq) -
+                   sum(self.pre_acq)) / (ACQ_SIZE * self.meas_pre_current)
+            r_b = (sum(self.zero_acq) -
+                   sum(self.after_acq)) / (ACQ_SIZE * self.meas_pre_current)
 
-        row = {
-            'step': round((self.acq_steps[-1] + 1) * self.v_period, 2),
-            'r_a': round(r_a, 4),
-            'r_b': round(r_b, 4),
-        }
-        self.tableModel.append(row)
+            row = {
+                'step': self.acq_steps[-1],
+                'r_a': round(r_a, 4),
+                'r_b': round(r_b, 4),
+            }
+            self.tableModel.append(row)
+
+    def _valid_data(self, data):
+        return data and data.lastval('is_on') and self._stable_current(
+            data, 0.01)
+
+    def _stable_current(self, data, tolerance):
+        return abs(data.lastval('current') -
+                   data.lastval('set_current')) < tolerance
 
     def _next_step(self, volt):
-        new_step = int(divmod(volt, self.v_period)[0])
-        if new_step not in self.acq_steps:
-            self.acq_steps.append(new_step)
+        v_period = self.v_period
+        new_step = int(divmod(volt, v_period)[0])
+        new_step_value = round((new_step + 1) * v_period, 2)
+        if new_step_value not in self.acq_steps:
+            self.acq_steps.append(new_step_value)
             return True
         return False
